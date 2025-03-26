0e9c3c70066bbf7afa31d02e4f355e778d3aa003
Ruslan Shuranov <ruslanshuranov@gmail.com>
2025-03-26 20:20:47 +0100

    Deploy to gh-pages

---
+++ build/precache-manifest.17eebbb51c7d717622fd9ef555cef4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6781ad455f97b33ba124b93c37f78c4c",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "feb472549f9777fc0a36",
+    "url": "/react-quiz/static/css/main.aac067b4.chunk.css"
+  },
+  {
+    "revision": "0b22aa7ebf218e83e800",
+    "url": "/react-quiz/static/js/2.dd56a5a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz/static/js/2.dd56a5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb472549f9777fc0a36",
+    "url": "/react-quiz/static/js/main.57f0465b.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  }
+]);
